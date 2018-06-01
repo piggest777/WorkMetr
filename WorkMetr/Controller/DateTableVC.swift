b262d01d7e490b634ff1829a4074ec16ae8a9665
Денис Ракитин <piggest@mail.ru>
2018-06-01 16:01:21 +0300

    init app

---
+++ WorkMetr/Controller/DateTableVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkMetr
+//
+//  Created by Denis Rakitin on 01/06/2018.
+//  Copyright © 2018 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+
+class DateTableVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
